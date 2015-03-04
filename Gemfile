58a03dbe0731f18a324d89202a47052403b537a5
Ian Hattendorf <ianhattendorf@gmail.com>
2015-03-04 15:29:47 -0700

    Add bootstrap

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 
 gem 'jquery-rails'
+gem 'bootstrap-sass', '~> 3.3.3'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  @base_title = 'Pastebin'
+
+  # Returns the full page title based on the current page
+  def full_title(page_title = '')
+    if page_title.empty?
+      @base_title
+    else
+      "#{@base_title} | #{page_title}"
+    end
+  end
 end
