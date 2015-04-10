8dadc767ddbed01fc8699b518122623721d67227
Ian Hattendorf <ianhattendorf@gmail.com>
2015-04-09 21:28:53 -0700

    Seed database with initial user and pastes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default
-# values. The data can then be loaded with the rake db:seed (or created alongside the db with
-# db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+ruby_content = <<PASTE_CONTENT
+require 'test_helper'
+
+describe User do
+  let(:user) do
+    User.create!(
+      email: 'test@example.com',
+      password: 'password',
+      password_confirmation: 'password')
+  end
+
+  it 'must be valid' do
+    user.must_be :valid?
+  end
+end
+PASTE_CONTENT
+
+text_content = <<PASTE_CONTENT
+Cornhole vegan meditation blog, mixtape readymade viral small batch Thundercats
+letterpress locavore. Fingerstache post-ironic yr, brunch crucifix McSweeney's
+cold-pressed umami retro ugh beard 90's selfies. Blog wolf small batch sriracha
+Neutra. Flannel farm-to-table scenester plaid pork belly shabby chic. Wolf
+American Apparel +1 biodiesel, cold-pressed sartorial Austin banjo.
+Farm-to-table aesthetic skateboard, yr YOLO bitters pour-over meh banh
+mi tousled dreamcatcher semiotics messenger bag freegan. Godard paleo American
+Apparel, viral Carles Intelligentsia vinyl.
+PASTE_CONTENT
+
+User.create(email: 'test@example.com', password: 'asdfasdf')
+
+Paste.create!(title: 'User Test', content: ruby_content, language: 'ruby')
+Paste.create!(title: 'So hip', content: text_content, language: 'text')
+Paste.create!(title: '', content: 'untitled paste', language: 'text')
